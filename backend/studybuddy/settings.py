4d9b81c30d2834a0caaba88d706f28bc947b91fc
ikramhamid2003 <ikramhamid4567@gmail.com>
2026-03-04 18:15:35 +0530

    fix cors with correct vercel url

--- backend/studybuddy/settings.py
+++ backend/studybuddy/settings.py
@@ -65,6 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://studybuddy-frontend-five.vercel.app",
+    "https://studybuddy-omega-gray.vercel.app",
 ]
 CORS_ALLOW_ALL_ORIGINS = False
 
