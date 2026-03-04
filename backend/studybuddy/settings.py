8b734ab3c89bf964df97bc838f94b54cd7fc1e9f
ikramhamid2003 <ikramhamid4567@gmail.com>
2026-03-04 17:52:13 +0530

    add vercel url to cors

--- backend/studybuddy/settings.py
+++ backend/studybuddy/settings.py
@@ -63,6 +63,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://studybuddy-frontend-five.vercel.app",
 ]
 CORS_ALLOW_ALL_ORIGINS = DEBUG
 
